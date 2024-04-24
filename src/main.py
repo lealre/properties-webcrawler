0d898b970d8b3b1a5c37cf8e1647fa21c518fb80
lealre <renaanvp@gmail.com>
2024-04-24 08:55:04 +0100

    feat: main.py working

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import sys
+import os
+# Add the root directory of your project to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+from request.generic_crawler import GenericCrawler
+
+df_sapo = GenericCrawler(site= 'sapo').crawl('tomar')
+print(df_sapo)
+
+df_imo = GenericCrawler(site = 'imovirtual').crawl('evora')
+print(df_imo)
