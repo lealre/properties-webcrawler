a9eddd5cc113ba8ac9000b5d85642a88d7814978
lealre <renaanvp@gmail.com>
2024-04-25 16:11:17 +0100

    test: response test to Redis keys links added

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -7,8 +7,21 @@ import pytest
 from src.tools.mongodb import MongoConnection
 from src.tools.redis import RedisClient
 
+@pytest.mark.dependency()
 def test_redis_connection():
+    """
+    Test the connection to Redis.
+
+    Raises:
+        AssertionError: If the Redis connection test fails.
+    """
     assert RedisClient().get().ping(), "Redis connection test failed"
 
 def test_mongo_connection():
+    """
+    Test the connection to MongoDB.
+    
+    Raises:
+        AssertionError: If the MongoDB connection test fails.
+    """
     assert MongoConnection().server_info(), "MongoDB connection test failed"
